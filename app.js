780ebf4dd28a3e001c8af1da2183fc2093738e4a
Kushi <maybesoumo@yahoo.com>
2024-10-18 14:29:54 +0530

    hm ho gaya

--- app.js
+++ app.js
@@ -1,54 +1,27 @@
 const TelegramBot = require('node-telegram-bot-api');
-const { exec } = require('child_process');
 
 const token = '7646877814:AAFx-LjNMqIqzLs-30pTwM_vVrV0w5DHDLA';
 const bot = new TelegramBot(token, { polling: true });
 
 const imageURL = 'https://i.ibb.co/XDPzBWc/pngtree-virtual-panel-generate-ai-image-15868619.jpg';
-const adminIds = [5430259245, 7019407723];
+const messageText = `Want to know how cool your Telegram presence is ? 
+Check your profile rating and unlock awesome rewards with $MTRX Matrix AI !
+
+Time to vibe ✨ and step into the world of Web3.
+$MTRX is on the way... Ready to explore something new ?
+
+Take the first step and see just how you stack up !`;
 
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const firstName = msg.from.first_name;
-    const username = msg.from.username ? `@${msg.from.username}` : firstName;
+  const chatId = msg.chat.id;
 
-    const messageText = `Hey ${username}, Want to know how cool your Telegram presence is ? 
-    Check your profile rating and unlock awesome rewards with $MTX Matrix AI !
-
-    Time to vibe ✨ and step into the world of Web3.
-    $MTX is on the way... Ready to explore something new ?
-
-    Take the first step and see just how you stack up !`;
-
-    bot.sendPhoto(chatId, imageURL, {
-        caption: messageText,
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: 'Play Now 🪂', web_app: { url: 'https://mtx-ai-bot.vercel.app' } }],
-                [{ text: 'Join Community 🔥', url: 'https://telegram.me/MatrixAi_Ann' }]
-            ]
-        }
-    });
-});
-
-bot.onText(/\/update/, (msg) => {
-    const chatId = msg.chat.id;
-
-    if (!adminIds.includes(msg.from.id)) {
-        return;
+  bot.sendPhoto(chatId, imageURL, {
+    caption: messageText,
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Play Now 🪂', web_app: { url: 'https://mtx-ai-bot.vercel.app' } }],
+        [{ text: 'Join Community 🔥', url: 'https://telegram.me/MatrixAi_Ann' }]
+      ]
     }
-
-    exec('git pull', (error, stdout, stderr) => {
-        if (error) {
-            bot.sendMessage(chatId, `Error: ${stderr}`);
-            return;
-        }
-
-        const uptimeInSeconds = process.uptime();
-        const uptimeInMinutes = Math.floor(uptimeInSeconds / 60);
-        const uptimeInHours = Math.floor(uptimeInMinutes / 60);
-        const uptimeFormatted = `${uptimeInHours} hours, ${uptimeInMinutes % 60} minutes`;
-
-        bot.sendMessage(chatId, `Update successful! Please restart the bot to apply changes.\nUptime: ${uptimeFormatted}`);
-    });
+  });
 });
